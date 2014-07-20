54321dcbba979cba2ae427edff6c286214c671df
Lito <lito@eordes.com>
2014-07-20 17:08:56 +0200

    Fixed default regular expression escape sign

--- FormManager/Attributes/Pattern.php
+++ FormManager/Attributes/Pattern.php
@@ -16,7 +16,7 @@ class Pattern {
 
 	public static function validate ($input) {
 		$value = $input->val();
-		$attr = str_replace('/', '\\', $input->attr('pattern'));
+		$attr = str_replace('/', '\\/', $input->attr('pattern'));
 
 		return (empty($attr) || empty($value) || filter_var($value, FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => '/'.$attr.'/')))) ? true : sprintf(static::$error_message, $attr);
 	}
