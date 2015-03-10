b834300a4d3690ffd616d01e3aa34d0f9fd7b0fc
oscarotero <oom@oscarotero.com>
2015-03-10 09:52:53 +0100

    new Loader container

---
+++ src/Containers/Loader.php
@@ -0,0 +1,51 @@
+<?php
+namespace FormManager\Containers;
+
+use FormManager\TreeInterface;
+
+class Loader extends Group
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function add(array $children)
+    {
+        $allowed = ['loader' => null, 'field' => null];
+
+        if (array_diff_key($children, $allowed)) {
+            throw new \InvalidArgumentException("Only 'loader' and 'field' keys are available.");
+        }
+
+        return parent::add($children);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load($value = null)
+    {
+        parent::load($value);
+
+        $loadedValue = $this['loader']->val();
+
+        if (!empty($loadedValue)) {
+            $this->val($loadedValue);
+        }
+
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function val($value = null)
+    {
+        if ($value === null) {
+            return $this['field']->val();
+        }
+
+        $this['field']->val($value);
+
+        return $this;
+    }
+}
